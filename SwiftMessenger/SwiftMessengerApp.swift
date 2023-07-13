9bbc38066b4e4815deb47777fd1bf267f2db8365
Javierb1219 <bonillaculture1219@gmail.com>
2023-07-12 19:25:48 -0700

    Initial Commit

---
+++ SwiftMessenger/SwiftMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMessengerApp.swift
+//  SwiftMessenger
+//
+//  Created by Javier Bonilla on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
